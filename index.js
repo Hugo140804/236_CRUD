2110b3956126cde0aafe59328b0bbd5737d1dc24
Hugo <hgugi3nsn@gmail.com>
2026-07-15 03:10:39 +0000

    GET

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mahasiswa',
+    password: 'admin123',
+    port: 5432,
+});
+
+app.get('/biodata', (req, res) => {
+    pool.query('SELECT * FROM biodata ORDER BY id ASC')
+        .then(result => {
+            res.json(result.rows);
+        })
+        .catch(err => {
+            console.error('Error executing query:', err.stack);
+            res.status(500).json({ error: 'Database error' });
+        });
+});
+
+app.get('/biodata/:id', (req, res) => {
+    const { id } = req.params;
+    pool.query('SELECT * FROM biodata WHERE id = $1', [id])
+        .then(result => {
+            if (result.rows.length === 0) {
+                return res.status(404).json({ message: 'Data tidak ditemukan' });
+            }
+            res.json(result.rows[0]);
+        })
+        .catch(err => {
+            console.error('Error executing query:', err.stack);
+            res.status(500).json({ error: 'Database error' });
+        });
+});
+
